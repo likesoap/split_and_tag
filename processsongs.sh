213833eecdb5ed13dede2b74397e2d182e592e54
likesoap <lightsaber@163.com>
2025-05-31 22:22:35 +0200

    change names

--- processsongs.sh
+++ processsongs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Create output directory
-output_dir="./processed/"
+output_dir="processed"
 
 required_cmds=("jq" "ffmpeg")
+
+sanitize_metadata() {
+    echo "$1" | iconv -c -f UTF-8 -t ASCII//TRANSLIT | tr -cd '[:alnum:]._ -'
+}
 
 for cmd in "${required_cmds[@]}"; do
     if ! command -v "$cmd" &>/dev/null; then
@@ -16,7 +20,6 @@ done
 while IFS= read -r name; do
     extracted_file_json="${name}.info.json"
     extracted_file="${name}.opus"
-    outfileName="${extracted_file}"
 
     # Check if the associated .opus file exists
     if [[ ! -f "$extracted_file" ]]; then
@@ -30,26 +33,30 @@ while IFS= read -r name; do
     track_number=$(jq -r '.playlist_index // 0' "$extracted_file_json")
     title=$(jq -r '.title // "Unknown Title"' "$extracted_file_json")
 
-    target_dir="./${output_dir}/${album}"
+    #define output path and filenames, removing the non regonizable characters
+    target_dir="./${output_dir}/$(sanitize_metadata "$album")"
+    output_file="$target_dir/$(sanitize_metadata "$name").opus"
 
-    # Check if the directory exists
+    # check if target dir exists
     if [ ! -d "$target_dir" ]; then
         echo "Directory does not exist. Creating: $target_dir"
         mkdir -p "$target_dir"
-    else
-        echo "Directory already exists: $target_dir"
-    fi
 
-    # Process audio with ffmpeg
-    ffmpeg -y -i "$extracted_file" -c copy \
-        -id3v2_version 3 \
-        -hide_banner \
-        -metadata title="$title" \
-        -metadata album="$album" \
-        -metadata artist="$artist" \
-        -metadata track="$track_number" \
-        -loglevel error \
-        "$target_dir/$outfileName"
+    # Skip if output file already exists
+    elif [[ ! -f "$output_file" ]]; then
+        ffmpeg -y -i "$extracted_file" -c copy \
+            -id3v2_version 3 \
+            -hide_banner \
+            -metadata title="$title" \
+            -metadata album="$album" \
+            -metadata artist="$artist" \
+            -metadata track="$track_number" \
+            -loglevel error \
+            "$output_file"
+    else
+        echo "Skipping '$outfileName' — already exists."
+        continue
+    fi
 
 done < <(find . -type f -name "*.info.json" -exec basename {} .info.json \;)
 
